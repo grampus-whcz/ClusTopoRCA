bd5211237e8bdf5ef8e42cf211c294218c8539d5
root <wuhecuizao@126.com>
2026-01-07 14:04:36 +0800

    some experiments

--- experiments_a.sh
+++ experiments_a.sh
@@ -12,23 +12,23 @@
 # nohup bash experiments_a.sh >> Market_cloudbed-1_all_RAG_gpt-4o.log 2>&1 &
 # python -m rca.run_agent_standard_multi_candidate --dataset Market/cloudbed-1 --controller_max_step 1 --start_idx 1  --end_idx 70
 
-# nohup bash experiments_a.sh >> Market_cloudbed-1_all_RAG_gpt-4o.log 2>&1 &
+# nohup bash experiments_a.sh >> Market_cloudbed-2_all_RAG_gpt-4o.log 2>&1 &
 # python -m rca.run_agent_standard_multi_candidate --dataset Market/cloudbed-2 --controller_max_step 1 --start_idx 1  --end_idx 78
 
 ## ====================================================================
 
 ## qwen3-235b-a22b-instruct-2507
 # nohup bash experiments_a.sh >> Bank_all_RAG_qwen3-235b-a22b-instruct-2507.log 2>&1 &
-# python -m rca.run_agent_standard_multi_candidate --dataset Bank --controller_max_step 1 --start_idx 1  --end_idx 136
+# python -m rca.run_agent_standard_multi_candidate --dataset Bank --controller_max_step 1 --start_idx 58  --end_idx 136
 
 # nohup bash experiments_a.sh >> Telecom_all_RAG_qwen3-235b-a22b-instruct-2507.log 2>&1 &
 # python -m rca.run_agent_standard_multi_candidate --dataset Telecom --controller_max_step 1 --start_idx 1  --end_idx 51
 
 # nohup bash experiments_a.sh >> Market_cloudbed-1_all_RAG_qwen3-235b-a22b-instruct-2507.log 2>&1 &
-# python -m rca.run_agent_standard_multi_candidate --dataset Market/cloudbed-1 --controller_max_step 1 --start_idx 1  --end_idx 70
+# python -m rca.run_agent_standard_multi_candidate --dataset Market/cloudbed-1 --controller_max_step 1 --start_idx 21  --end_idx 70
 
-# nohup bash experiments_a.sh >> Market_cloudbed-1_all_RAG_qwen3-235b-a22b-instruct-2507.log 2>&1 &
-# python -m rca.run_agent_standard_multi_candidate --dataset Market/cloudbed-2 --controller_max_step 1 --start_idx 1  --end_idx 78
+# nohup bash experiments_a.sh >> Market_cloudbed-2_all_RAG_qwen3-235b-a22b-instruct-2507.log 2>&1 &
+python -m rca.run_agent_standard_multi_candidate --dataset Market/cloudbed-2 --controller_max_step 1 --start_idx 1  --end_idx 78
 
 ## ====================================================================
 
@@ -37,12 +37,12 @@
 # python -m rca.run_agent_standard_multi_candidate --dataset Bank --controller_max_step 1 --start_idx 1  --end_idx 136
 
 # nohup bash experiments_a.sh >> Telecom_all_RAG_deepseek-r1-250528.log 2>&1 &
-python -m rca.run_agent_standard_multi_candidate --dataset Telecom --controller_max_step 1 --start_idx 1  --end_idx 51
+# python -m rca.run_agent_standard_multi_candidate --dataset Telecom --controller_max_step 1 --start_idx 1  --end_idx 51
 
 # nohup bash experiments_a.sh >> Market_cloudbed-1_all_RAG_deepseek-r1-250528.log 2>&1 &
 # python -m rca.run_agent_standard_multi_candidate --dataset Market/cloudbed-1 --controller_max_step 1 --start_idx 1  --end_idx 70
 
-# nohup bash experiments_a.sh >> Market_cloudbed-1_all_RAG_deepseek-r1-250528.log 2>&1 &
+# nohup bash experiments_a.sh >> Market_cloudbed-2_all_RAG_deepseek-r1-250528.log 2>&1 &
 # python -m rca.run_agent_standard_multi_candidate --dataset Market/cloudbed-2 --controller_max_step 1 --start_idx 1  --end_idx 78
 
 ## ====================================================================
@@ -57,5 +57,5 @@ python -m rca.run_agent_standard_multi_candidate --dataset Telecom --controller_
 # nohup bash experiments_a.sh >> Market_cloudbed-1_all_RAG_gemini-2.5-pro-preview-p.log 2>&1 &
 # python -m rca.run_agent_standard_multi_candidate --dataset Market/cloudbed-1 --controller_max_step 1 --start_idx 1  --end_idx 70
 
-# nohup bash experiments_a.sh >> Market_cloudbed-1_all_RAG_gemini-2.5-pro-preview-p.log 2>&1 &
+# nohup bash experiments_a.sh >> Market_cloudbed-2_all_RAG_gemini-2.5-pro-preview-p.log 2>&1 &
 # python -m rca.run_agent_standard_multi_candidate --dataset Market/cloudbed-2 --controller_max_step 1 --start_idx 1  --end_idx 78
